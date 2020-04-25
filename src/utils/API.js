1f67eac8aa420b60ad8c1afbb2860d098f63f3ce
sprea22 <a.spreafico13@campus.unimib.it>
2020-04-25 14:23:28 +0200

    BaseURL changed

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: `https://localhost:5000/prediction/`
+        baseURL: `https://localhost/prediction/`
     })
 }
