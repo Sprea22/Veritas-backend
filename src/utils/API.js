e80f897c14b5c05c383721a7639374d82334d4ae
sprea22 <a.spreafico13@campus.unimib.it>
2020-04-25 14:31:53 +0200

    Procfile updated

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: `https://localhost/prediction/`
+        baseURL: `https://localhost:5000/prediction/`
     })
 }
---
+++ start_script.sh
@@ -0,0 +1,2 @@
+cd Server
+flask run
